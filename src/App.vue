3c56aa31678f8b0137a96dd6576f3d8038f33b29
Tav2001 <anhvan20142015@gmail.com>
2023-03-29 11:57:43 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+} 
+</script>
+
+<template>
+  <h1> Hello, Vue.js! </h1>
+</template>
+
+<style>
+.page {
+  max-width: 400px;
+  margin: auto;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+</style>
